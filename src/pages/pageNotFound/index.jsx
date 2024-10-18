b73dabd7d0bd2d1389595e557da75c1d5f25217e
saurabh-gaonkar <saurabhg@axioned.com>
2024-10-18 13:35:27 +0530

    added: fallback page

---
+++ src/pages/pageNotFound/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import StyledPageNotFound from './pageNotFound.styled'
+
+const PageNotFound = () => {
+  return (
+    <StyledPageNotFound>Page Not Found</StyledPageNotFound>
+  )
+}
+
+export default PageNotFound
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/homepage";
 import BookDetails from "../pages/bookDetails";
+import PageNotFound from "../pages/pageNotFound";
 const RoutesComponent = () => {
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/book/:id" element={<BookDetails />} />
+        <Route exact path="*" element={<PageNotFound />} />
       </Routes>
     </>
   );
